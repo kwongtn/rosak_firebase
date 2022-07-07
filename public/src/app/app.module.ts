dc40afdf2abb15c46b0111b75eedfe6a47ec0414
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-07 18:19:03 +0800

    Init angular firebase & devxUI
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevUIModule } from 'ng-devui';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DevUIModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
