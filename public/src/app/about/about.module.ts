0e45e7413339fc56278c3670602e19493750ddc0
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-17 01:10:09 +0800

    Add about page component & module
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ public/src/app/about/about.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { AboutComponent } from "./about/about.component";
+
+@NgModule({
+    declarations: [AboutComponent],
+    imports: [CommonModule],
+})
+export class AboutModule {}
