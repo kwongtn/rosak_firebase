c88dc4f527409f5ce0d6a87274cc0cd0615cd7a0
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-17 20:54:25 +0800

    Add firebase authentication
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 
+import { AuthService } from "./services/auth/auth.service";
+
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
@@ -23,7 +25,7 @@ export class AppComponent {
         },
     ];
 
-    constructor() {
+    constructor(public authService: AuthService) {
         return;
     }
 }
--- public/src/app/services/auth/auth.service.ts
+++ public/src/app/services/auth/auth.service.ts
@@ -1,10 +1,75 @@
+import firebase from "firebase/compat/app";
+import { BehaviorSubject } from "rxjs";
+
 import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+
+import { ToastService } from "../toast/toast.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class AuthService {
-    constructor() {
+    userData: BehaviorSubject<firebase.User | null> =
+        new BehaviorSubject<firebase.User | null>(null);
+
+    constructor(
+        private angularFireAuth: AngularFireAuth,
+        private toastService: ToastService
+    ) {
         return;
+    }
+
+    login() {
+        this.angularFireAuth
+            .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+            .then((res) => {
+                this.userData.next(res.user);
+
+                let toastMessage: string;
+                if (
+                    res.additionalUserInfo &&
+                    res.additionalUserInfo.isNewUser
+                ) {
+                    toastMessage = "Welcome!";
+                } else {
+                    toastMessage = "Welcome back.";
+                }
+
+                this.toastService.addToast({
+                    severity: "success",
+                    summary: "Login Successful",
+                    content: toastMessage,
+                });
+                console.log(res);
+            })
+            .catch((reason) => {
+                console.log("Login failed: ", reason);
+                this.toastService.addToast({
+                    severity: "error",
+                    summary: "Login Error",
+                    content: reason.message,
+                });
+            });
+    }
+
+    logout() {
+        this.angularFireAuth
+            .signOut()
+            .then(() => {
+                this.userData.next(null);
+                this.toastService.addToast({
+                    severity: "success",
+                    summary: "Logout Successful",
+                });
+            })
+            .catch((reason) => {
+                console.log("Logout failed: ", reason);
+                this.toastService.addToast({
+                    severity: "error",
+                    summary: "Logout Error",
+                    content: reason.message,
+                });
+            });
     }
 }
