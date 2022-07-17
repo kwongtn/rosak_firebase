2b9eededa392bbfcc7e3feab68a880e9c3e140fb
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-17 19:21:40 +0800

    Add firebase deps
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { ToastModule } from "ng-devui/toast";
 
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { environment } from "../environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { GraphQLModule } from "./graphql.module";
@@ -15,6 +21,12 @@ import { SpottingModule } from "./spotting/spotting.module";
 @NgModule({
     declarations: [AppComponent],
     imports: [
+        // TODO: AnalyticsModule
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFireStorageModule,
+        AngularFireDatabaseModule,
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
