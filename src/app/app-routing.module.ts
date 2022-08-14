8ddcf383b09c87ace38f56226d9241e7dbd5b470
Kwong Tung Nan <tungnan5636@gmail.com>
2022-08-14 16:10:31 +0800

    Make site under construction
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AboutComponent } from "./about/about/about.component";
+import { ConstructionComponent } from "./construction/construction.component";
 import { FallbackComponent } from "./fallback/fallback.component";
-import {
-    SpottingMainComponent,
-} from "./spotting/spotting-main/spotting-main.component";
 
 const routes: Routes = [
     {
@@ -13,22 +11,32 @@ const routes: Routes = [
         data: {
             title: "'MLPTF | Spotting'",
         },
+        // loadChildren: async () => {
+        //     const module = await import("./spotting/spotting.module");
+        //     return module.SpottingModule;
+        // },
+        // component: SpottingMainComponent,
         loadChildren: async () => {
-            const module = await import("./spotting/spotting.module");
-            return module.SpottingModule;
+            const module = await import("./construction/construction.module");
+            return module.ConstructionModule;
         },
-        component: SpottingMainComponent,
+        component: ConstructionComponent,
     },
     {
         path: "spotting/:id",
         data: {
             title: "'MLPTF | Spotting'",
         },
+        // loadChildren: async () => {
+        //     const module = await import("./spotting/spotting.module");
+        //     return module.SpottingModule;
+        // },
+        // component: SpottingMainComponent,
         loadChildren: async () => {
-            const module = await import("./spotting/spotting.module");
-            return module.SpottingModule;
+            const module = await import("./construction/construction.module");
+            return module.ConstructionModule;
         },
-        component: SpottingMainComponent,
+        component: ConstructionComponent,
     },
     {
         path: "about",
