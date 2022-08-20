8d512c0ad195e97d31a24201fb17d0589bb50ecf
Kwong Tung Nan <tungnan5636@gmail.com>
2022-08-20 17:30:05 +0800

    Initial inline history implementation
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/spotting/vehicle-type-container/spotting-table/inline-history/inline-history.component.ts
+++ src/app/spotting/vehicle-type-container/spotting-table/inline-history/inline-history.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { TableWidthConfig } from "ng-devui";
+
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-inline-history",
@@ -6,6 +8,53 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./inline-history.component.scss"],
 })
 export class InlineHistoryComponent implements OnInit {
+    @Input() vehicleId: string = "1";
+
+    data = [
+        {
+            spottingDate: "2022-08-07",
+            status: "IN_SERVICE",
+            type: "JUST_SPOTTING",
+            notes: "",
+        },
+    ];
+
+    dataTableOptions = {
+        columns: [
+            {
+                field: "date",
+                header: "spottingDate",
+                fieldType: "date",
+                order: 1,
+            },
+            {
+                field: "status",
+                header: "Status",
+                fieldType: "status",
+                order: 2,
+            },
+            {
+                field: "type",
+                header: "Type",
+                fieldType: "text",
+                order: 3,
+            },
+            {
+                field: "notes",
+                header: "Notes",
+                fieldType: "text",
+                order: 4,
+            },
+        ],
+    };
+
+    tableWidthConfig: TableWidthConfig[] = [
+        { field: "date", width: "100px" },
+        { field: "status", width: "150px" },
+        { field: "type", width: "150px" },
+        { field: "notes", width: "500px" },
+    ];
+
     constructor() {
         return;
     }
