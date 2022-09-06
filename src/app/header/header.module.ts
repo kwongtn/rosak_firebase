c5426d134fd65e5a00d33a3301be6307a217392f
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-06 08:43:55 +0800

    Copy theme picker module
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,16 @@
+import { RadioModule } from "ng-devui";
 import { AvatarModule } from "ng-devui/avatar";
 import { BadgeModule } from "ng-devui/badge";
 import { ButtonModule } from "ng-devui/button";
 import { CardModule } from "ng-devui/card";
 import { DropDownModule } from "ng-devui/dropdown";
+import { TabsModule } from "ng-devui/tabs";
+import { ToggleModule } from "ng-devui/toggle";
 import { TooltipModule } from "ng-devui/tooltip";
 
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 
 import { HeaderComponent } from "./header/header.component";
@@ -36,6 +40,10 @@ import { ThemeSwitchComponent } from "./theme-switch/theme-switch.component";
         TooltipModule,
         CardModule,
         ButtonModule,
+        TabsModule,
+        RadioModule,
+        ToggleModule,
+        FormsModule,
     ],
     exports: [HeaderComponent, LogoComponent, MenuComponent],
 })
