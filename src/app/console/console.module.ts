9dd6847c32877223c026716a00742688f0bce8a4
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-26 23:13:00 +0800

    Initialize consoleEventsTable component
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/console/console.module.ts
+++ src/app/console/console.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import {
+    ConsoleEventsTableComponent,
+} from "./events-table/events-table.component";
 import { ConsoleMainComponent } from "./main/main.component";
 
 @NgModule({
-    declarations: [ConsoleMainComponent],
+    declarations: [ConsoleMainComponent, ConsoleEventsTableComponent],
     imports: [CommonModule],
 })
 export class ConsoleModule {}
---
+++ src/app/console/events-table/events-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "console-events-table",
+    templateUrl: "./events-table.component.html",
+    styleUrls: ["./events-table.component.scss"],
+})
+export class ConsoleEventsTableComponent implements OnInit {
+    constructor() {
+        return;
+    }
+
+    ngOnInit(): void {
+        return;
+    }
+}
