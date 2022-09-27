7ad8a139f15eb76298401e15a29a00601ae9a3f2
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-27 22:53:46 +0800

    Add data to console main component
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/console/main/main.component.ts
+++ src/app/console/main/main.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+
+import { Component, OnDestroy, OnInit } from "@angular/core";
+
+import {
+    ConsoleEventsGqlResponse,
+    ConsoleEventsGqlService,
+} from "../services/events-gql/events-gql.service";
 
 @Component({
     selector: "app-console-main",
     templateUrl: "./main.component.html",
     styleUrls: ["./main.component.scss"],
 })
-export class ConsoleMainComponent implements OnInit {
-    constructor() {
+export class ConsoleMainComponent implements OnInit, OnDestroy {
+    eventGqlSubscription!: Subscription;
+
+    tableData: ConsoleEventsGqlResponse = {
+        eventsLastThreeDays: [],
+        eventsLastFiveDaysHasNotes: [],
+        eventsLastSevenDaysDifferentStatusThanVehicle: [],
+    };
+
+    constructor(private consoleEventsGqlService: ConsoleEventsGqlService) {
         return;
     }
 
     ngOnInit(): void {
-        return;
+        this.eventGqlSubscription = this.consoleEventsGqlService
+            .watch({
+                eventsLastThreeDaysFilters: {
+                    daysBefore: 3,
+                },
+                eventsLastFiveDaysHasNotesFilters: {
+                    daysBefore: 5,
+                    hasNotes: true,
+                },
+                eventsLastSevenDaysDifferentStatusThanVehicleFilters: {
+                    daysBefore: 7,
+                    differentStatusThanVehicle: true,
+                },
+            })
+            .valueChanges.subscribe(({ data, loading }) => {
+                this.tableData = data;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.eventGqlSubscription?.unsubscribe();
     }
 }
