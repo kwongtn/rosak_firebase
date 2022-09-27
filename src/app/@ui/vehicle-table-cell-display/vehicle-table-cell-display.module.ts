0b45bb7f39b32dfbfb70ac7646325281c6df4f44
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-27 23:12:20 +0800

    Add vehicleStatusTag to vehicleTableCell
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/@ui/vehicle-table-cell-display/vehicle-table-cell-display.module.ts
+++ src/app/@ui/vehicle-table-cell-display/vehicle-table-cell-display.module.ts
@@ -4,12 +4,19 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import {
+    VehicleStatusTagModule,
+} from "../vehicle-status-tag/vehicle-status-tag.module";
+import {
     VehicleTableCellDisplayComponent,
 } from "./vehicle-table-cell-display.component";
 
 @NgModule({
     declarations: [VehicleTableCellDisplayComponent],
-    imports: [CommonModule, TooltipModule],
+    imports: [
+        CommonModule,
+        TooltipModule,
+        VehicleStatusTagModule,
+    ],
     exports: [VehicleTableCellDisplayComponent],
 })
 export class VehicleTableCellDisplayModule {}
