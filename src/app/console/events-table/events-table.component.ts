33c5174555bb583cc0ce15f4d33b8f1ec20b7cc9
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-27 22:43:03 +0800

    Add table to consoleEventsTableComponent
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/console/events-table/events-table.component.ts
+++ src/app/console/events-table/events-table.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { TableWidthConfig } from "ng-devui";
+
+import { Component, Input, OnInit } from "@angular/core";
+
+import {
+    ConsoleEventsGqlResponseElement,
+} from "../services/events-gql/events-gql.service";
 
 @Component({
     selector: "console-events-table",
@@ -6,6 +12,65 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./events-table.component.scss"],
 })
 export class ConsoleEventsTableComponent implements OnInit {
+    @Input() dataSource!: ConsoleEventsGqlResponseElement[];
+
+    dataTableOptions = {
+        columns: [
+            {
+                field: "id",
+                header: "Event ID",
+                fieldType: "id",
+                order: 1,
+            },
+            {
+                field: "created",
+                header: "Created",
+                fieldType: "datetime",
+                order: 2,
+            },
+            {
+                field: "status",
+                header: "Status",
+                fieldType: "status",
+                order: 3,
+            },
+            {
+                field: "spottingDate",
+                header: "Date",
+                fieldType: "text",
+                order: 4,
+            },
+            {
+                field: "type",
+                header: "Spotting Type",
+                fieldType: "text",
+                order: 5,
+            },
+            {
+                field: "vehicle",
+                header: "Vehicle",
+                fieldType: "vehicle",
+                order: 6,
+            },
+            {
+                field: "notes",
+                header: "Notes",
+                fieldType: "text",
+                order: 7,
+            },
+        ],
+    };
+
+    tableWidthConfig: TableWidthConfig[] = [
+        { field: "id", width: "100px" },
+        { field: "created", width: "150px" },
+        { field: "status", width: "150px" },
+        { field: "spottingDate", width: "150px" },
+        { field: "type", width: "150px" },
+        { field: "vehicle", width: "250px" },
+        { field: "notes", width: "500px" },
+    ];
+
     constructor() {
         return;
     }
