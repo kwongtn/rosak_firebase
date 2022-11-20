9aab60a87b80ecf9341ef964247de4e2564f60b5
Kwong Tung Nan <tungnan5636@gmail.com>
2022-11-20 12:39:36 +0800

    Add fixedDigit to coordinatesHumanizer
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/pipes/coordinates-humanizer/coordinates-humanizer.pipe.ts
+++ src/app/pipes/coordinates-humanizer/coordinates-humanizer.pipe.ts
@@ -4,13 +4,15 @@ import { Pipe, PipeTransform } from "@angular/core";
     name: "coordinatesHumanizer",
 })
 export class CoordinatesHumanizerPipe implements PipeTransform {
-    transform(value: GeolocationCoordinates): string {
+    transform(value: GeolocationCoordinates, toFixedCount: number = 5): string {
         console.log(value);
         const latSign = value.latitude > 0 ? "N" : "S";
         const lonSign = value.longitude > 0 ? "E" : "W";
 
-        return `${Math.abs(value.latitude).toFixed(5)}${latSign}, ${Math.abs(
-            value.longitude
-        ).toFixed(5)}${lonSign} ± ${value.accuracy.toFixed(5)}m`;
+        return `${Math.abs(value.latitude).toFixed(
+            toFixedCount
+        )}${latSign}, ${Math.abs(value.longitude).toFixed(
+            toFixedCount
+        )}${lonSign} ± ${value.accuracy.toFixed(toFixedCount)}m`;
     }
 }
