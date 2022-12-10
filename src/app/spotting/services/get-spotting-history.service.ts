bf723461325ea077a2078bf7b2bd3c75ed723757
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-10 17:57:26 +0800

    Shift getSpottingHistory to service & add infinity loading
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ src/app/spotting/services/get-spotting-history.service.ts
@@ -0,0 +1,38 @@
+import { gql, Query } from "apollo-angular";
+import {
+    GetVehiclesLastSpottingResponse,
+} from "src/app/models/query/get-vehicles";
+
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root",
+})
+export class GetSpottingHistoryService extends Query<GetVehiclesLastSpottingResponse> {
+    override document = gql`
+        query (
+            $eventFilters: EventFilter
+            $eventPagination: OffsetPaginationInput
+            $eventOrder: EventOrder
+        ) {
+            events(
+                filters: $eventFilters
+                pagination: $eventPagination
+                order: $eventOrder
+            ) {
+                spottingDate
+                status
+                type
+                notes
+                location {
+                    accuracy
+                    altitudeAccuracy
+                    heading
+                    speed
+                    location
+                    altitude
+                }
+            }
+        }
+    `;
+}
--- src/app/spotting/vehicle-type-container/spotting-table/inline-history/inline-history.component.ts
+++ src/app/spotting/vehicle-type-container/spotting-table/inline-history/inline-history.component.ts
@@ -1,39 +1,15 @@
-import { Apollo, gql } from "apollo-angular";
-import { TableWidthConfig } from "ng-devui";
+import { QueryRef } from "apollo-angular";
+import { DataTableComponent, TableWidthConfig } from "ng-devui";
 import { Subscription } from "rxjs";
 import {
     GetVehiclesLastSpottingResponse,
     LastSpottingsTableElement,
 } from "src/app/models/query/get-vehicles";
+import {
+    GetSpottingHistoryService,
+} from "src/app/spotting/services/get-spotting-history.service";
 
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
-
-const GET_VEHICLE_LAST_SPOTTINGS = gql`
-    query (
-        $eventFilters: EventFilter
-        $eventPagination: OffsetPaginationInput
-        $eventOrder: EventOrder
-    ) {
-        events(
-            filters: $eventFilters
-            pagination: $eventPagination
-            order: $eventOrder
-        ) {
-            spottingDate
-            status
-            type
-            notes
-            location {
-                accuracy
-                altitudeAccuracy
-                heading
-                speed
-                location
-                altitude
-            }
-        }
-    }
-`;
 
 @Component({
     selector: "app-inline-history",
@@ -46,6 +22,7 @@ export class InlineHistoryComponent implements OnInit, OnDestroy {
     // Pagination
     limit = 30;
     offset = 0;
+    watchQueryOption!: QueryRef<GetVehiclesLastSpottingResponse>;
 
     showLoading: boolean = true;
     querySubscription!: Subscription;
@@ -88,45 +65,77 @@ export class InlineHistoryComponent implements OnInit, OnDestroy {
         { field: "notes", width: "500px" },
     ];
 
-    constructor(private apollo: Apollo) {
+    constructor(private getSpottingHistoryGql: GetSpottingHistoryService) {
         return;
     }
 
     ngOnInit(): void {
-        this.querySubscription = this.apollo
-            .query<GetVehiclesLastSpottingResponse>({
-                query: GET_VEHICLE_LAST_SPOTTINGS,
+        this.watchQueryOption = this.getSpottingHistoryGql.watch(
+            {
+                eventFilters: {
+                    vehicleId: this.vehicleId,
+                },
+                eventPagination: {
+                    limit: this.limit,
+                    offset: this.offset,
+                },
+                eventOrder: {
+                    spottingDate: "DESC",
+                },
+            },
+            {
+                fetchPolicy: "network-only",
+            }
+        );
+
+        this.querySubscription = this.watchQueryOption.valueChanges.subscribe(
+            ({ data, loading }) => {
+                this.showLoading = false;
+                this.dataSource = this.mapGqlResultsToDisplayData(data);
+
+                this.offset = this.dataSource.length;
+            }
+        );
+    }
+
+    loadMore($event: DataTableComponent) {
+        this.showLoading = true;
+
+        this.watchQueryOption
+            .fetchMore({
                 variables: {
-                    eventFilters: {
-                        vehicleId: this.vehicleId,
-                    },
                     eventPagination: {
                         limit: this.limit,
                         offset: this.offset,
                     },
-                    eventOrder: {
-                        spottingDate: "DESC",
-                    },
                 },
             })
-            .subscribe(({ data, loading }) => {
+            .then(({ data, loading }) => {
+                this.dataSource = this.dataSource.concat(
+                    this.mapGqlResultsToDisplayData(data)
+                );
+
                 this.showLoading = false;
-                this.dataSource = data.events.map((val) => {
-                    const returnObj: any = {
-                        ...val,
-                    };
-
-                    if (val.location) {
-                        returnObj.location = {
-                            ...val.location,
-                            latitude: val.location.location[1],
-                            longitude: val.location.location[0],
-                        };
-                    }
-
-                    return returnObj;
-                });
+                this.offset = this.dataSource.length;
             });
+    }
+
+    mapGqlResultsToDisplayData(data: any) {
+        return data.events.map((val: any) => {
+            const returnObj: any = {
+                ...val,
+            };
+
+            if (val.location) {
+                returnObj.location = {
+                    ...val.location,
+                    latitude: val.location.location[1],
+                    longitude: val.location.location[0],
+                };
+            }
+
+            return returnObj;
+        });
     }
 
     ngOnDestroy() {
