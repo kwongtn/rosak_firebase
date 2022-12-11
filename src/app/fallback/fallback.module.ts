2bb5f6318700745057879e4065597cd9dae4238f
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-11 16:50:57 +0800

    chore(fallback): Remove unused routing component
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/fallback/fallback.module.ts
+++ src/app/fallback/fallback.module.ts
@@ -3,11 +3,10 @@ import { ButtonModule } from "ng-devui/button";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
-import { FallbackRoutingModule } from "./fallback-routing.module";
 import { FallbackComponent } from "./fallback.component";
 
 @NgModule({
     declarations: [FallbackComponent],
-    imports: [CommonModule, FallbackRoutingModule, ButtonModule],
+    imports: [CommonModule, ButtonModule],
 })
 export class FallbackModule {}
