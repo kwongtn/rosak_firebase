a18a0d5d3a39f692b22f428edbc173fcfb9f0789
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-16 11:32:22 +0800

    feat(spotting): Add trends component
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ src/app/profile/spotting-trends/spotting-trends.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "profile-spotting-trends",
+    templateUrl: "./spotting-trends.component.html",
+    styleUrls: ["./spotting-trends.component.scss"],
+})
+export class SpottingTrendsComponent implements OnInit {
+    constructor() {
+        return;
+    }
+
+    ngOnInit(): void {
+        return;
+    }
+}
