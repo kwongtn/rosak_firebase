ee46dcf2a3e421d2c62e9de1f0ac19b2c64c84b0
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-17 15:13:44 +0800

    feat(user-trend): Modify query for chart data
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/profile/services/get-user-data.service.ts
+++ src/app/profile/services/get-user-data.service.ts
@@ -2,11 +2,22 @@ import { gql, Query } from "apollo-angular";
 
 import { Injectable } from "@angular/core";
 
+export interface UserSpottingTrends {
+    spottingDate: string | null;
+    year: number | null;
+    month: number | null;
+    day: number | null;
+    count: number;
+}
+
+export interface UserDataResponseUser {
+    firebaseId: string;
+    spottingsCount: number;
+    spottingTrends: UserSpottingTrends[];
+}
+
 export interface UserDataResponse {
-    user: {
-        firebaseId: string;
-        spottingsCount: number;
-    };
+    user: UserDataResponseUser;
 }
 
 @Injectable({
@@ -14,10 +25,18 @@ export interface UserDataResponse {
 })
 export class GetUserDataService extends Query<UserDataResponse> {
     override document = gql`
-        query {
+        query ($dateGroup: DateGroupings, $typeGroup: Boolean) {
             user {
                 firebaseId
                 spottingsCount
+                spottingTrends(dateGroup: $dateGroup, typeGroup: $typeGroup) {
+                    spottingDate
+                    year
+                    month
+                    day
+                    eventType
+                    count
+                }
             }
         }
     `;
