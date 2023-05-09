f00b1330497a47c06e768e7ab8c8fc97380f31b0
Kwong Tung Nan <tungnan5636@gmail.com>
2023-05-10 02:20:22 +0800

    ux(general): Scroll to top on navigation
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -184,7 +184,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes, {
+            scrollPositionRestoration: "enabled",
+        }),
+    ],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
