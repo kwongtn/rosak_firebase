098fc9edc6486c5557f5b32d67f49d0a55e10ed0
Kwong Tung Nan <tungnan5636@gmail.com>
2023-07-08 16:29:49 +0800

    fix(spotting-form): Optimized slow operations when no lines are selected
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/spotting/spotting-form/spotting-form.component.ts
+++ src/app/spotting/spotting-form/spotting-form.component.ts
@@ -128,6 +128,10 @@ export class SpottingFormComponent implements OnInit, OnDestroy {
     vehicleSearchFn = (
         term: string
     ): Observable<{ id: string | number; option: any }[]> => {
+        if (!this.formGroup["value"]?.line?.value) {
+            return of([]);
+        }
+
         const setNumber = numberSeenToSetNumber(
             term,
             this.formGroup.value.line?.value
