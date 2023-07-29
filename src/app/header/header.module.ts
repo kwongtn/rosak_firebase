a6bf86481354eac81163003c94c7a7bfe3c19949
Kwong Tung Nan <tungnan5636@gmail.com>
2023-07-29 15:25:42 +0800

    feat(header): Login dropdown follow-system-theme switch to use ng implementation
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -4,11 +4,11 @@ import { BadgeModule } from "ng-devui/badge";
 import { ButtonModule } from "ng-devui/button";
 import { CardModule } from "ng-devui/card";
 import { DropDownModule } from "ng-devui/dropdown";
-import { ToggleModule } from "ng-devui/toggle";
 import { TooltipModule } from "ng-devui/tooltip";
 import { NzAlertModule } from "ng-zorro-antd/alert";
 import { NzBadgeModule } from "ng-zorro-antd/badge";
 import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzSwitchModule } from "ng-zorro-antd/switch";
 
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
@@ -40,13 +40,13 @@ import { MenuComponent } from "./menu/menu.component";
         ButtonModule,
         CardModule,
         DropDownModule,
-        ToggleModule,
         TooltipModule,
 
         // Ng-Zorro
         NzAlertModule,
         NzBadgeModule,
         NzIconModule,
+        NzSwitchModule,
     ],
     exports: [HeaderComponent, LogoComponent, MenuComponent],
 })
