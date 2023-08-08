f6a0e1eee42d652aee7afec41b68e61996a866bd
Kwong Tung Nan <tungnan5636@gmail.com>
2023-08-08 11:11:39 +0800

    feat(situasi): Panels to adapt to theme
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/situasi/situasi.component.ts
+++ src/app/situasi/situasi.component.ts
@@ -1,8 +1,18 @@
+import { Observable } from "rxjs";
+
 import { Component } from "@angular/core";
+
+import { ThemeService } from "../services/theme/theme.service";
 
 @Component({
     selector: "app-situasi",
     templateUrl: "./situasi.component.html",
     styleUrls: ["./situasi.component.scss"],
 })
-export class SituasiComponent {}
+export class SituasiComponent {
+    $theme!: Observable<"light" | "dark">;
+
+    constructor(themeService: ThemeService) {
+        this.$theme = themeService.colorScheme.asObservable();
+    }
+}
--- src/app/situasi/situasi.module.ts
+++ src/app/situasi/situasi.module.ts
@@ -1,3 +1,7 @@
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { FooterModule } from "src/app/@ui/footer/footer.module";
+
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
@@ -5,6 +9,6 @@ import { SituasiComponent } from "./situasi.component";
 
 @NgModule({
     declarations: [SituasiComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, NzLayoutModule, NzMenuModule, FooterModule],
 })
 export class SituasiModule {}
