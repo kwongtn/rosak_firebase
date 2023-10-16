a1786bf427ca81a8f6244d816feb45d1066bfc9d
Kwong Tung Nan <tungnan5636@gmail.com>
2023-10-16 11:22:58 +0800

    feat(session-history): Add session history service
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ src/app/services/session-history/session-history.service.ts
@@ -0,0 +1,43 @@
+import { BehaviorSubject } from "rxjs";
+
+import { Injectable } from "@angular/core";
+
+type THistoryStore = "spotting" | "mediaUpload";
+
+type IHistoryStore = {
+    [timestamp: number]: {
+        historyType: THistoryStore;
+        [key: string]: any;
+    };
+};
+
+@Injectable({
+    providedIn: "root",
+})
+export class SessionHistoryService {
+    historyStore: BehaviorSubject<IHistoryStore> =
+        new BehaviorSubject<IHistoryStore>({});
+
+    historyStoreLength: BehaviorSubject<number> = new BehaviorSubject<number>(
+        0
+    );
+
+    addSessionHistory(type: THistoryStore, data: any): void {
+        this.historyStore.next({
+            ...this.historyStore.value,
+            [new Date().getTime()]: data,
+        });
+    }
+
+    clearSessionHistory(): number {
+        const len = Object.keys(this.historyStore.value).length;
+        this.historyStore.next({});
+        return len;
+    }
+
+    constructor() {
+        this.historyStore.subscribe((val) => {
+            this.historyStoreLength.next(Object.keys(val).length);
+        });
+    }
+}
