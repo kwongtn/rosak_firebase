908335b51736dd452d3266c5eb6ab09af1d9afe8
Kwong Tung Nan <tungnan5636@gmail.com>
2023-11-01 08:10:21 +0800

    rrefactor(situasi): Move things out of sub-component to cater for lazy-loading
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/situasi/content/content.component.ts
+++ src/app/situasi/content/content.component.ts
@@ -1,9 +1,4 @@
-import { Subscription } from "rxjs";
-
-import { Component, Input, OnDestroy } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-
-import { data } from "../data";
+import { Component } from "@angular/core";
 
 export interface BreadcrumbsData {
     displayText: string;
@@ -15,79 +10,4 @@ export interface BreadcrumbsData {
     templateUrl: "./content.component.html",
     styleUrls: ["./content.component.scss"],
 })
-export class ContentComponent implements OnDestroy {
-    @Input() title: string = "Title";
-    @Input() subtitle: string = "subtitle";
-
-    lineId!: string;
-    assetType!: string;
-    assetId!: string;
-
-    titleString!: string;
-
-    menuData = data;
-
-    @Input() breadcrumbsData: BreadcrumbsData[] = [];
-
-    private routeSubscription!: Subscription;
-
-    constructor(private route: ActivatedRoute) {
-        this.routeSubscription = this.route.params.subscribe((params: any) => {
-            console.log(params);
-            this.lineId = params["lineId"];
-            this.assetType = params["assetType"];
-            this.assetId = params["assetId"];
-
-            this.breadcrumbsData = [];
-
-            const currentLine = this.menuData.find((elem) => {
-                return elem.id === this.lineId;
-            });
-
-            if (currentLine) {
-                this.breadcrumbsData.push({
-                    displayText: currentLine.displayName,
-                    href: [this.lineId],
-                });
-
-                if (this.assetType) {
-                    const assetVerb: "vehicles" | "stations" = (this.assetType +
-                        "s") as any; // Assume is 'vehicles' or'stations'
-                    this.breadcrumbsData.push({
-                        displayText:
-                            assetVerb.charAt(0).toUpperCase() +
-                            assetVerb.slice(1),
-                        href: [this.lineId, this.assetType],
-                    });
-
-                    const displayAsset = currentLine[assetVerb].find(
-                        (asset) => {
-                            return this.assetId === asset.id;
-                        }
-                    );
-
-                    if (displayAsset) {
-                        this.titleString = displayAsset.displayName;
-
-                        this.breadcrumbsData.push({
-                            displayText: this.titleString,
-                            href: [this.lineId, this.assetType, this.assetId],
-                        });
-                    } else {
-                        this.titleString =
-                            currentLine.displayName +
-                            " - " +
-                            assetVerb.charAt(0).toUpperCase() +
-                            assetVerb.slice(1);
-                    }
-                } else {
-                    this.titleString = currentLine.displayName;
-                }
-            }
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.routeSubscription?.unsubscribe();
-    }
-}
+export class ContentComponent {}
--- src/app/situasi/situasi.component.ts
+++ src/app/situasi/situasi.component.ts
@@ -1,8 +1,13 @@
 import { NzMenuItemDirective } from "ng-zorro-antd/menu";
-import { Observable } from "rxjs";
+import { filter, Observable, Subscription } from "rxjs";
 
 import { Component } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import {
+    ActivatedRoute,
+    DefaultUrlSerializer,
+    NavigationEnd,
+    Router,
+} from "@angular/router";
 
 import { ThemeService } from "../services/theme/theme.service";
 import { BreadcrumbsData } from "./content/content.component";
@@ -18,15 +23,18 @@ export class SituasiComponent {
 
     menuData = data;
 
-    lineId: string = "";
-    assetType: string = "";
-    assetId: string = "";
+    lineId: string | undefined = undefined;
+    assetType: string | undefined = undefined;
+    assetId: string | undefined = undefined;
 
     displayLineString!: string;
     displayAssetString!: string;
+    titleString!: string;
 
     breadcrumbsData: BreadcrumbsData[] = [];
     router: Router;
+
+    private routeSubscription!: Subscription;
 
     constructor(
         themeService: ThemeService,
@@ -35,9 +43,80 @@ export class SituasiComponent {
     ) {
         this.$theme = themeService.colorScheme.asObservable();
         this.router = router;
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe((event) => {
+                const url = (event as NavigationEnd).url;
+                const segments = new DefaultUrlSerializer().parse(url).root
+                    .children["primary"].segments;
+
+                this.lineId =
+                    segments.length > 1 ? segments[1].path : undefined;
+                this.assetType =
+                    segments.length > 2 ? segments[2].path : undefined;
+                this.assetId =
+                    segments.length > 3 ? segments[3].path : undefined;
+
+                this.breadcrumbsData = [];
+
+                const currentLine = this.menuData.find((elem) => {
+                    return elem.id === this.lineId;
+                });
+
+                if (currentLine && this.lineId) {
+                    this.breadcrumbsData.push({
+                        displayText: currentLine.displayName,
+                        href: [this.lineId],
+                    });
+
+                    if (this.assetType) {
+                        const assetVerb: "vehicles" | "stations" = (this
+                            .assetType + "s") as any; // Assume is 'vehicles' or'stations'
+                        this.breadcrumbsData.push({
+                            displayText:
+                                assetVerb.charAt(0).toUpperCase() +
+                                assetVerb.slice(1),
+                            href: [this.lineId, this.assetType],
+                        });
+
+                        const displayAsset = currentLine[assetVerb].find(
+                            (asset) => {
+                                return this.assetId === asset.id;
+                            }
+                        );
+
+                        if (displayAsset && this.assetId) {
+                            this.titleString = displayAsset.displayName;
+
+                            this.breadcrumbsData.push({
+                                displayText: this.titleString,
+                                href: [
+                                    this.lineId,
+                                    this.assetType,
+                                    this.assetId,
+                                ],
+                            });
+                        } else {
+                            this.titleString =
+                                currentLine.displayName +
+                                " - " +
+                                assetVerb.charAt(0).toUpperCase() +
+                                assetVerb.slice(1);
+                        }
+                    } else {
+                        this.titleString = currentLine.displayName;
+                    }
+                } else {
+                    this.titleString = "Situasi";
+                }
+            });
     }
 
     onMenuItemClick(event: NzMenuItemDirective): void {
         console.log(event);
+    }
+
+    ngOnDestroy(): void {
+        this.routeSubscription?.unsubscribe();
     }
 }
