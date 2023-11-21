131ec74d5134759455743b3e2dd9119539c05063
Kwong Tung Nan <tungnan5636@gmail.com>
2023-11-21 20:30:46 +0800

    feat(spo-lch): Shift data retrieval to service

--- src/app/@ui/spotting-line-calendar-heatmap/spotting-line-calendar-heatmap.component.ts
+++ src/app/@ui/spotting-line-calendar-heatmap/spotting-line-calendar-heatmap.component.ts
@@ -1,7 +1,7 @@
-import { environment } from "src/environments/environment";
-
-import { Component, HostListener, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Chart } from "@antv/g2";
+
+import { GetDataService } from "./get-data/get-data.service";
 
 @Component({
     selector: "spotting-line-calendar-heatmap",
@@ -13,18 +13,12 @@ export class SpottingLineCalendarHeatmapComponent implements OnInit {
 
     chart: Chart | undefined = undefined;
 
-    getNewChartWidthHeight(): [number, number] {
-        return [window.innerWidth - 300, window.innerHeight - 200];
+    getNewChartWidthHeight(rowCount: number): [number, number] {
+        const MULTIPLIER = 50; // 50px per unique value
+        return [1600, rowCount * MULTIPLIER];
     }
 
-    @HostListener("window:resize")
-    resize(): void {
-        const [width, height] = this.getNewChartWidthHeight();
-        this.chart?.changeSize(width, height);
-        this.chart?.render();
-    }
-
-    constructor() {
+    constructor(private getDataService: GetDataService) {
         return;
     }
 
@@ -36,74 +30,80 @@ export class SpottingLineCalendarHeatmapComponent implements OnInit {
 
         const endDateString = new Date().toISOString().split("T")[0];
 
-        const [width, height] = this.getNewChartWidthHeight();
+        this.getDataService
+            .getData(this.lineId, startDateString, endDateString)
+            .then((data) => {
+                const vehicleSet = new Set(data.map((item) => item.vehicle));
+                const [width, height] = this.getNewChartWidthHeight(
+                    vehicleSet.size
+                );
 
-        this.chart = new Chart({
-            scrollbar: true,
-            container: "container",
-            type: "view",
-            data: {
-                type: "fetch",
-                value: `${environment.backendUrl}operation/line_vehicles_spotting_trend/${this.lineId}/${startDateString}/${endDateString}/`,
-                format: "json",
-            },
-            width: 1600,
-            height: 2000,
-            scale: {
-                color: {
-                    palette: "puRd",
-                    relations: [
-                        [(d: string) => d === null, "#eee"],
-                        [0, "#fff"],
-                    ],
-                },
-            },
-            axis: {
-                y: { labelAutoRotate: false, title: "Set Number" },
-                x: {
-                    tickFilter: (d: string) => {
-                        const week = d.split("-W")[1];
-                        return Number.parseInt(week) % 3 === 0;
+                this.chart = new Chart({
+                    scrollbar: true,
+                    container: "container",
+                    type: "view",
+                    data: {
+                        type: "inline",
+                        value: data,
                     },
-                    position: "top",
-                    title: "Week of Year",
-                },
-            },
-            children: [
-                {
-                    type: "cell",
-                    encode: {
-                        x: "dateKey",
-                        y: "vehicle",
-                        color: "count",
-                    },
-                    style: { inset: 0.5 },
-                    tooltip: {
-                        title: {
-                            channel: "y",
+                    width,
+                    height,
+                    scale: {
+                        color: {
+                            palette: "puRd",
+                            relations: [
+                                [(d: string) => d === null, "#eee"],
+                                [0, "#fff"],
+                            ],
                         },
-                        items: [
-                            {
-                                name: "Week of Year",
-                                channel: "x",
-                                field: "dateKey",
-                            },
-                            {
-                                name: "Vehicle",
-                                channel: "y",
-                                field: "vehicle",
-                            },
-                            {
-                                name: "Count",
-                                channel: "color",
-                                field: "count",
-                            },
-                        ],
                     },
-                },
-            ],
-        });
+                    axis: {
+                        y: { labelAutoRotate: false, title: "Set Number" },
+                        x: {
+                            tickFilter: (d: string) => {
+                                const week = d.split("-W")[1];
+                                return Number.parseInt(week) % 3 === 0;
+                            },
+                            position: "top",
+                            title: "Week of Year",
+                        },
+                    },
+                    children: [
+                        {
+                            type: "cell",
+                            encode: {
+                                x: "dateKey",
+                                y: "vehicle",
+                                color: "count",
+                            },
+                            style: { inset: 0.5 },
+                            tooltip: {
+                                title: {
+                                    channel: "y",
+                                },
+                                items: [
+                                    {
+                                        name: "Week of Year",
+                                        channel: "x",
+                                        field: "dateKey",
+                                    },
+                                    {
+                                        name: "Vehicle",
+                                        channel: "y",
+                                        field: "vehicle",
+                                    },
+                                    {
+                                        name: "Count",
+                                        channel: "color",
+                                        field: "count",
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                });
 
-        this.chart.render();
+                this.chart.render();
+            });
     }
 }
