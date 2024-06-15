6af9876b5eb47189b3df26904a08850a32f90f04
Kwong Tung Nan <tungnan5636@gmail.com>
2024-06-15 19:16:09 +0800

    dev(t): Panel realtime selections to its own component
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ src/app/tracker/status-card/panel-rt-layer/panel-rt-layer.component.ts
@@ -0,0 +1,48 @@
+import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
+import { NzCollapsePanelComponent } from "ng-zorro-antd/collapse";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzListModule } from "ng-zorro-antd/list";
+import { NzProgressModule } from "ng-zorro-antd/progress";
+import { NzSpinModule } from "ng-zorro-antd/spin";
+import { NzToolTipModule } from "ng-zorro-antd/tooltip";
+
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+
+import { GtfsStateService } from "../../services/gtfs-state.service";
+import {
+    ICollapseItem,
+    PanelSelectionService,
+    PanelType,
+} from "../../services/panel-selection.service";
+
+@Component({
+    selector: "tracker-panel-rt-layer",
+    standalone: true,
+    imports: [
+        CommonModule,
+        NzCheckboxModule,
+        NzCollapsePanelComponent,
+        NzIconModule,
+        NzListModule,
+        NzProgressModule,
+        NzSpinModule,
+        NzToolTipModule,
+    ],
+    templateUrl: "./panel-rt-layer.component.html",
+    styleUrl: "./panel-rt-layer.component.scss",
+})
+export class PanelRtLayerComponent {
+    @Input() panelKey!: string;
+    @Input() panelData!: ICollapseItem;
+
+    constructor(
+        public panelSelectionService: PanelSelectionService,
+        public gtfsStateService: GtfsStateService
+    ) {}
+
+    updateChecked(id: string, index: number) {
+        console.log(id, index);
+        this.panelSelectionService.toggleChecked(id as PanelType, index);
+    }
+}
