aaad7cf8a7247fac389612215604ddf5b406c0d3
Kwong Tung Nan <tungnan5636@gmail.com>
2025-03-10 09:50:42 +0800

    fix: Verification component not displaying code properly
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- src/app/@ui/verification-code-card/verification-code-card.component.ts
+++ src/app/@ui/verification-code-card/verification-code-card.component.ts
@@ -1,10 +1,11 @@
 import { AuthService } from "src/app/services/auth.service";
 
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component } from "@angular/core";
 
+import { firstValueFrom } from "rxjs";
 import { GetCodeService } from "./services/get-code.service";
 
-const VERIFICATION_COUNTDOWN= 60;
+const VERIFICATION_COUNTDOWN = 60;
 
 @Component({
     selector: "verification-code-card",
@@ -12,8 +13,8 @@ const VERIFICATION_COUNTDOWN= 60;
     styleUrl: "./verification-code-card.component.scss",
 })
 export class VerificationCodeCardComponent {
-    verificationCode: number| undefined = undefined;
-    codeExpiry: Date| undefined = undefined;
+    verificationCode: number | undefined = undefined;
+    codeExpiry: Date | undefined = undefined;
 
     countdown: number = VERIFICATION_COUNTDOWN;
 
@@ -22,41 +23,50 @@ export class VerificationCodeCardComponent {
     isLoading = false;
 
     constructor(
-      public authService: AuthService,
-      public getCodeService: GetCodeService,
+        public authService: AuthService,
+        public getCodeService: GetCodeService,
+        private cdr: ChangeDetectorRef
     ) {
         return;
     }
 
-    async getCode(){
+    async getCode() {
         const authKey = await this.authService.getIdToken();
-
         this.isLoading = true;
-        this.getCodeService.mutate({}, {
-            context: {
-                headers: {
-                    "firebase-auth-key": authKey,
-                },
-            },
-            fetchPolicy: "network-only",
-        }).subscribe((res) => {
-            this.verificationCode = res.data?.requestVerificationCode.code;
-            this.codeExpiry = new Date(new Date().getTime() + 6e4);
-            this.isLoading = false;
 
-            this.interval = setInterval(() => {
-                this.countdown--;
-
-                if(this.countdown === 0){
-                    clearInterval(this.interval);
-    
-                    this.verificationCode = undefined;
-                    this.codeExpiry = undefined;
-                    this.countdown = VERIFICATION_COUNTDOWN;
+        firstValueFrom(
+            this.getCodeService.mutate(
+                {},
+                {
+                    context: {
+                        headers: {
+                            "firebase-auth-key": authKey,
+                        },
+                    },
+                    fetchPolicy: "network-only",
                 }
-            }, 1000);
-            
-        });
-    }
+            )
+        )
+            .then((res) => {
+                this.verificationCode = res.data?.requestVerificationCode.code;
+                this.codeExpiry = new Date(new Date().getTime() + 6e4);
 
+                this.interval = setInterval(() => {
+                    this.countdown--;
+
+                    if (this.countdown === 0) {
+                        clearInterval(this.interval);
+
+                        this.verificationCode = undefined;
+                        this.codeExpiry = undefined;
+                        this.countdown = VERIFICATION_COUNTDOWN;
+                    }
+                    this.cdr.detectChanges();
+                }, 1000);
+            })
+            .finally(() => {
+                this.isLoading = false;
+                this.cdr.detectChanges();
+            });
+    }
 }
